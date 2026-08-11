0a54ef973feca9304e1829395f64a468aec69c9c
Leo <109210868+4nt1r34l@users.noreply.github.com>
2026-08-11 21:15:48 +0530

    Update: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -1,3 +1,3 @@
 class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums+nums
+        def hasDuplicate(self, nums: List[int]) -> bool:
+            return len(nums) != len(set(nums))
