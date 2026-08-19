9161743f97315d002740deae6a852cb634f9644b
Leo <109210868+4nt1r34l@users.noreply.github.com>
2026-08-19 13:30:50 +0530

    Update: surrounded-regions - submission-0

--- Data Structures & Algorithms/surrounded-regions/submission-0.py
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -1,36 +1,35 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        ROWS = len(board)
-        COLS = len(board[0])
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        queue = deque([])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        rows, cols = len(board), len(board[0])
 
-        def explore(r,c):
-            row_in = 0 <= r < ROWS
-            col_in = 0 <= c < COLS
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and (r==0 or r==rows-1 or c == 0 or c == cols-1):
+                    queue.append((r,c))
+        
+        while queue:
+            r,c = queue.popleft()
 
-            if not row_in or not col_in or board[r][c] != 'O':
-                return
-            
             board[r][c] = 'T'
-            explore(r+1,c)
-            explore(r-1,c)
-            explore(r,c-1)
-            explore(r,c+1)
 
-        for r in range(ROWS):
-            if board[r][0] == '0':
-                explore(r,c)
-            if board[r][COLS-1] == '0':
-                explore(r,c)
+            for dr, dc in directions:
+                nr, nc = dr+r, dc+c
+                if 0<=nr<rows and 0<=nc<cols and board[nr][nc] == 'O':
+                    queue.append((nr,nc))
         
-        for c in range(COLS):
-            if board[0][c] == '0':
-                explore(r,c)
-            if board[ROWS-1][c] == '0':
-                explore(r,c)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
+
+
+
+
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
