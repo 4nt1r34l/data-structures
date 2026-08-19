d50a4b8abce8bb9157827bd4c15586c445f9c331
Leo <109210868+4nt1r34l@users.noreply.github.com>
2026-08-19 14:01:28 +0530

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,11 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
+        freq = defaultdict(list)
 
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            if sorted_word in anagrams:
-                anagrams[sorted_word].append(word)
-            else:
-                anagrams[sorted_word] = [word]
-        return list(anagrams.values())
+        for i in range(len(strs)):
+            word = "".join(sorted(strs[i]))
+            freq[word].append(strs[i])
+        
+        return list(freq.values())
