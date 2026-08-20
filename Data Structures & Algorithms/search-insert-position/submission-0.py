173f2144dfa81c865104cb4e6128e1a88a56a861
Leo <109210868+4nt1r34l@users.noreply.github.com>
2026-08-20 00:56:42 -0700

    Update: search-insert-position - submission-0

--- Data Structures & Algorithms/search-insert-position/submission-0.py
+++ Data Structures & Algorithms/search-insert-position/submission-0.py
@@ -3,12 +3,13 @@ class Solution:
         left, right = 0, len(nums)-1
 
         while left<=right:
-            mid = left + ((right-left)//2)
+            mid = (left+right)//2
 
             if nums[mid] == target:
                 return mid
-            elif nums[mid]>target:
-                right = mid-1
-            else:
+            elif nums[mid] < target:
                 left = mid+1
+            else:
+                right = mid-1
+        
         return left
