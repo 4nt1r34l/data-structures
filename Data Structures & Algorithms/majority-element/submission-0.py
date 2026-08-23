c035cb7922149bbf28ada85a3a1369a011adb156
Leo <109210868+4nt1r34l@users.noreply.github.com>
2026-08-23 08:03:41 -0700

    Update: majority-element - submission-0

--- Data Structures & Algorithms/majority-element/submission-0.py
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -1,7 +1,4 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        ans = [x for x in c if c[x]>len(nums)//2]
-        return ans[0]
-        
+        nums.sort()
+        return nums[len(nums)//2]
