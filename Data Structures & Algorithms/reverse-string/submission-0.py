9fa8ecc2f4157bc267ee548dce3a1ce1a936bfe4
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-01 01:58:30 +0530

    Update: reverse-string - submission-0

--- Data Structures & Algorithms/reverse-string/submission-0.py
+++ Data Structures & Algorithms/reverse-string/submission-0.py
@@ -3,5 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s[::-1]
-        
+        left, right = 0, len(s)-1
+        while left<right:
+            s[left], s[right] = s[right], s[left]
+            left+=1
+            right-=1
+            
