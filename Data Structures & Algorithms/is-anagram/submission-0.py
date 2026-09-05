df005ae3dd89b926f93052ee61653ba42cce4d76
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-05 10:30:43 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -2,10 +2,15 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        
+        freq = Counter(s)
 
-        countS, countT = {}, {}
-
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+        for char in t:
+            if char not in freq:
+                return False
+            
+            freq[char] -= 1
+            if freq[char] == 0:
+                del freq[char]
+        
+        return True
